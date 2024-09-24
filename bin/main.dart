0da2594dff2ed8fc528c646c6335f12dca6985ca
Shofiqur Rahman Soyon <shofiqur7684@gmail.com>
2024-09-25 00:15:04 +0600

    new feature added

--- bin/main.dart
+++ bin/main.dart
@@ -16,13 +16,12 @@ void main(){
   book2.read(20);
   book3.read(0);
 
-  print('Book1: ${book1.getTitle()} by ${book1.getAuthor()} Published: ${book1.getPublicationYear()}, PagesRead: ${book1.getPagesRead()}, AgeOfBook: ${book1.getBookAge()}\n');
-  print('Book1: ${book2.getTitle()} by ${book2.getAuthor()} Published: ${book2.getPublicationYear()}, PagesRead: ${book2.getPagesRead()}, AgeOfBook: ${book2.getBookAge()}\n');
-  print('Book1: ${book3.getTitle()} by ${book3.getAuthor()} Published: ${book3.getPublicationYear()}, PagesRead: ${book3.getPagesRead()}, AgeOfBook: ${book3.getBookAge()}\n');
+  print('Book1: ${book1.getTitle()} by ${book1.getAuthor()}, Published: ${book1.getPublicationYear()}, PagesRead: ${book1.getPagesRead()}, AgeOfBook: ${book1.getBookAge()}.\n');
+  print('Book1: ${book2.getTitle()} by ${book2.getAuthor()}, Published: ${book2.getPublicationYear()}, PagesRead: ${book2.getPagesRead()}, AgeOfBook: ${book2.getBookAge()}.\n');
+  print('Book1: ${book3.getTitle()} by ${book3.getAuthor()}, Published: ${book3.getPublicationYear()}, PagesRead: ${book3.getPagesRead()}, AgeOfBook: ${book3.getBookAge()}.\n');
   
   print('Total Books: ${Book.totalBooks}');
 
-  print(book1.getPagesRead());
   
 
 
